b9cd9afd2c9668063d55f89919bfa85e22d1592d
rs6brd-creator <rs6brd@gmail.com>
2026-04-27 01:05:24 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("AI Video App شغال 🔥");
-});
+app.use(express.static(__dirname));
 
-app.listen(3000, () => console.log("Server running"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
