344454fb7299b079b21357b018e987ec4a0c0d32
rs6brd-creator <rs6brd@gmail.com>
2026-04-27 01:16:48 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,27 @@ const path = require("path");
 
 const app = express();
 
+// مهم لمعالجة البيانات
+app.use(express.json());
+
+// لعرض الملفات (index.html)
 app.use(express.static(__dirname));
 
+// API لما تضغط الزر
+app.post("/generate", (req, res) => {
+  const text = req.body.text;
+
+  console.log("Received:", text);
+
+  res.json({
+    message: "تم إنشاء الفيديو 🎬",
+    text: text
+  });
+});
+
+// تشغيل السيرفر
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
